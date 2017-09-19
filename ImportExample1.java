7b99236070c3deec2098478e0f7a447cf38f7dbe
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-19 21:56:09 +0530

    To illustrate the concept of import in java.

---
+++ ImportExample1.java
@@ -0,0 +1,8 @@
+class ImportExample1
+{
+  public static void main(String []args)
+  {
+      // ArrayList l=new ArrayList();  -> CE:cannot find symbol:ArrayList
+      java.util.ArrayList l=new java.util.ArrayList();
+  }
+}
