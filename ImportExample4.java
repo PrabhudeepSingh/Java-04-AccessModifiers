a62ae44073bee5de8736ea76df4ed8f5a602bd7c
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-19 21:56:26 +0530

    To illustrate the concept of import in java.

---
+++ ImportExample4.java
@@ -0,0 +1,10 @@
+import java.util.Date;
+import java.sql.*;
+public class ImportExample4
+{
+  public static void main(String []args)
+  {
+    Date d=new Date();
+    System.out.println(d.getClass().getName()); //util package Date get considered
+  }
+}
