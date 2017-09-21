33a80d075cc5d29f1e797c4afca4b873478442f4
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-21 21:30:02 +0530

    Reference file.

---
+++ A.java
@@ -0,0 +1,9 @@
+package pack2;
+//private class A//CE:modifier private not allowed here
+public class A//if class is public than it can be access that class from anywhere
+{
+  public void m1()
+  {
+     System.out.println("hello");
+  }
+}
