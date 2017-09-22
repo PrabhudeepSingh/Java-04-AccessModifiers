d88a8b016a763d2fba6a4ae05aa618519d813768
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-22 10:46:05 +0530

    To illustrate the condition when main method isn't present in a java class.

---
+++ Prabhudeep.java
@@ -0,0 +1,25 @@
+//save this file as Prabhudeep.java
+class A
+{
+  public static void main(String []args)
+    {
+      System.out.println("A class main");
+    }
+}
+class B
+{
+  public static void main(String []args)
+    {
+      System.out.println("B class main");
+    }
+}
+class C
+{
+  public static void main(String []args)
+    {
+      System.out.println("C class main");
+    }
+}
+class D
+{
+} //RE: Main method not found in class D
