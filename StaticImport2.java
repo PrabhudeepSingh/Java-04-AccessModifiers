b4a44d002d1a811932b69b3ae28e905043cec080
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-22 10:42:31 +0530

    To illustrate the concept of static import in java.

---
+++ StaticImport2.java
@@ -0,0 +1,13 @@
+import static java.lang.Math.sqrt;
+import static java.lang.Math.max;
+import static java.lang.System.out;
+public class StaticImport2
+{
+  public static void main(String []args)
+  {
+   println(sqrt(4));
+   println(max(10,30));
+   println(random());
+  }
+}
+//with static import
