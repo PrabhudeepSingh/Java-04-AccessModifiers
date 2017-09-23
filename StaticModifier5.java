35709b21a9dd9bb0dccb37d40bb31ec443535bc2
PrabhudeepSingh <prabhudeepsb@gmail.com>
2017-09-23 22:20:05 +0530

    To illustrate the concept of static Modifier in java.

---
+++ StaticModifier5.java
@@ -0,0 +1,17 @@
+class StaticModifier5
+{
+  public static void main(String[] args)
+  {
+    System.out.println("parent class");
+  }
+}
+
+class A extends StaticModifier5
+{
+  
+}
+
+//Output's:
+
+//java StaticModifier5   -> parent class
+//java A                 -> parent class
